4470537d03eca7969f7fbfe5ddb39ceb8afa9409
karimzgithub <159678565+karimzgithub@users.noreply.github.com>
2025-02-21 12:42:41 +0530

    Add files via upload

---
+++ Day12_Assignment1.py
@@ -0,0 +1,14 @@
+#creating tuple
+tuplex =(2,4,5,6,2,3,4,4,7)
+print(tuplex)
+
+# counting occurance of number using count() function
+countx = tuplex.count(4)
+
+#print result
+print("No. of time 4 appeared: ",countx)
+
+'''OUTPUT:
+(2, 4, 5, 6, 2, 3, 4, 4, 7)
+No. of time 4 appeared:  3
+'''
---
+++ Day12_Assignment2.py
@@ -0,0 +1,15 @@
+# Sample list
+mylist = [5, 10, 7, 4, 15, 3]
+print("my list: ",mylist)
+
+# Convert list to tuple using tuple() function
+mytuple = tuple(mylist)
+
+# Print the result
+
+print("Converted List to Tuple:", mytuple)
+
+'''OUTPUT:
+my list:  [5, 10, 7, 4, 15, 3]
+Converted List to Tuple: (5, 10, 7, 4, 15, 3)
+'''
